ac32983dda11b988242383da3e7aff1876b48e45
Jason Dreyzehner <jason@dreyzehner.com>
2016-04-05 18:36:16 -0400

    chore(app-base): update dependencies, app-base

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: [
     path.normalize('es6-shim/es6-shim.min'),
     'reflect-metadata',
-    path.normalize('zone.js/dist/zone-microtask'),
+    path.normalize('zone.js/dist/zone'),
     path.resolve('app/app')
   ],
   output: {
@@ -49,7 +49,7 @@ module.exports = {
     noParse: [
       /es6-shim/,
       /reflect-metadata/,
-      /zone\.js(\/|\\)dist(\/|\\)zone-microtask/
+      /zone\.js(\/|\\)dist(\/|\\)zone/
     ]
   },
   resolve: {
